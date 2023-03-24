0e5a1be439d85d57cddcb7f69969d5d0c90c0d0b
Mystical94 <53875398+Mystical94@users.noreply.github.com>
2023-03-24 11:15:53 +0530

    Create custom_calling.go

---
+++ custom_package/test/custom_calling.go
@@ -0,0 +1,20 @@
+package main
+
+//import the custom package calculator
+import (
+	"fmt"
+	//"package/calculator"
+	"package/calculator"
+)
+
+func main() {
+
+	num1 := 9
+	num2 := 5
+
+	//use the add function of calculator package
+	fmt.Println(calculator.Add(num1, num2))
+
+	//use the add function of calculator package
+	fmt.Println(calculator.Substract(num1, num2))
+}
