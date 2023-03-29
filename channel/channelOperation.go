034d00ab06e57abff251f7f3b4b380a520be8b91
Mystical94 <53875398+Mystical94@users.noreply.github.com>
2023-03-29 17:12:28 +0530

    go channel

---
+++ channel/channelOperation.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// create channel
+	number := make(chan int)
+	message := make(chan string)
+
+	// function call with goroutine
+	go channelData(number, message)
+
+	// retrieve channel data
+	fmt.Println("Channel Data:", <-number)
+	fmt.Println("Channel Data", <-message)
+}
+
+func channelData(number chan int, message chan string) {
+
+	// send data into channel
+	number <- 10
+	message <- "Learning GoChannel"
+}
